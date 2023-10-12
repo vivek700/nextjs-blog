2dfc9e5a80e7017f1a71a78f69602f43b4e57d8c
Vivek Singh <vs690799@gmail.com>
2023-10-12 21:03:02 +0530

    first nextjs app

---
+++ components/date.js
@@ -0,0 +1,14 @@
+import { parseISO, format } from "date-fns";
+
+
+
+export default function Date({ dateString }) {
+    
+    const date = parseISO(dateString)
+    
+    return (
+        
+        <time dateTime={dateString}>{format(date, 'MMMM d, yyyy')}</time>
+    )
+
+}
